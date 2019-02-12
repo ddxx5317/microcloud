3da021aa1348dea7df278c1bd2a6fbc0239099d9
ddxx5317 <ddxx5317@163.com>
2019-02-12 23:05:01 +0800

    eureka

--- microcloud-provider-dept-8001/src/main/java/com/ddxx/microcloud/Dept_8001_StartSpringCloudApplication.java
+++ microcloud-provider-dept-8001/src/main/java/com/ddxx/microcloud/Dept_8001_StartSpringCloudApplication.java
@@ -2,8 +2,10 @@ package com.ddxx.microcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Dept_8001_StartSpringCloudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Dept_8001_StartSpringCloudApplication.class, args);
