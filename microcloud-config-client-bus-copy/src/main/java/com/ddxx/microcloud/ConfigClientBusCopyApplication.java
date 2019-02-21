828f9237f2f6f4f5ff479334129ab58de75f0bf9
ddxx <ddxx5317@163.com>
2019-02-21 17:18:58 +0800

    config bus

---
+++ microcloud-config-client-bus-copy/src/main/java/com/ddxx/microcloud/ConfigClientBusCopyApplication.java
@@ -0,0 +1,11 @@
+package com.ddxx.microcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientBusCopyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientBusCopyApplication.class, args);
+	}
+}
