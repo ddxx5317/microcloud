c4d32cbb5c2a007e34a0c5395f9285ed20c598bc
ddxx <ddxx5317@163.com>
2019-02-25 11:31:51 +0800

    bus-eureka

---
+++ microcloud-config-client-bus-enreka/src/main/java/com/ddxx/microcloud/ConfigClientBusEnrekaApplication.java
@@ -0,0 +1,11 @@
+package com.ddxx.microcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientBusEnrekaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientBusEnrekaApplication.class, args);
+	}
+}
---
+++ microcloud-config-client-bus-enreka/src/main/java/com/ddxx/microcloud/controller/ConfigClientController.java
@@ -0,0 +1,37 @@
+package com.ddxx.microcloud.controller;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ConfigClientController {
+	
+	@Value("${profile}")
+	private String profile;
+	
+	
+	@Value("${url}")
+	private String url;
+	
+	@Value("${password}")
+	private String password;
+
+	@GetMapping("/profile")
+	public String hello() {
+		return this.profile+":"+StringUtils.trimToEmpty(this.url);
+	}
+	
+	@GetMapping("/encrypt")
+	public String encrypt() {
+		return this.profile+":"+StringUtils.trimToEmpty(this.url)+":"+this.password;
+	}
+	
+	@GetMapping("/decrypt")
+	public String decrypt() {
+		return this.profile+":"+StringUtils.trimToEmpty(this.url)+":"+this.password;
+	}
+}
